d16462a816f42475e54b91e4779800bc9de49663
ksimali <cassim.ali@outlook.fr>
2024-01-17 17:14:37 -0500

    Setting up ObtenirValeurString() function qui retourne la chaine saisie par le user: -Creation de la methode -Appel de la methode

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Main {
+    /*
+    * Method obtenirValeurString(Scanner scanner, String message) qui retourne la chaine saise par l'utilisateur
+    * */
+    public static String obtenirValeurString(Scanner scanner, String message){
+        System.out.println(message);
+        String str = scanner.nextLine();
+        return str;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Création du scanner pour lire depuis le clavier
+        Scanner scanner = new Scanner(System.in);
+
+        //Saisi du prénom
+        obtenirValeurString(scanner,"Entrez votre prénom: ");
+
+        //Saisi du  nom
+        obtenirValeurString(scanner, "Entrez votre nom: ");
+
     }
 }
